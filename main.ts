bedba6b2310163e025ff5a4f1ed0e3a401200d92
Elyes-Oughlici <83344459+Elyes-Oughlici@users.noreply.github.com>
2021-04-28 12:54:32 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(compte)
+})
+input.onButtonPressed(Button.AB, function () {
+    compte = 0
+})
+input.onButtonPressed(Button.B, function () {
+    compte += 1
+    basic.showNumber(compte)
+})
+let compte = 0
+compte = 0
